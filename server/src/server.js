d6b865757b735349f85c9bbfefa1143276d688f7
HRISTINA STOYANOVA <viara68@abv.bg>
2024-02-21 14:08:45 +0200

    Add logger middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,19 @@
+const express = require('express');
 const mongoose = require('mongoose');
+
+const expressConfigurator = require('./config/expressConfigurator');
 const dbConnect = require('./config/dbConfigurator');
 // const { logEvents } = require('./middlewares/logger');
 const routesConfigurator = require('./config/routesConfigurator');
+
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
 dbConnect(app);
+expressConfigurator(app);
 routesConfigurator(app);
+
 mongoose.connection.once('open', () => {
     console.log('DB connected successfully!');
     
