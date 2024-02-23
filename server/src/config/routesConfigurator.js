426e6edbc2a8498bc5c68567bbbb311f26f7f472
HRISTINA STOYANOVA <viara68@abv.bg>
2024-02-23 15:53:29 +0200

    Update routesConfigurator.js

--- server/src/config/routesConfigurator.js
+++ server/src/config/routesConfigurator.js
@@ -1,11 +1,11 @@
 const homeRouter = require('../routes/homeRoutes');
-const patientRouter = require('../routes/patientRouter');
-const appointmentRouter = require('../routes/appointmentRouter');
+const patientRouter = require('../routes/patientRoutes');
+// const appointmentRouter = require('../routes/appointmentRouter');
 const errorHandler = require('../middlewares/errorHandler');
 
 module.exports = (app) => {
     app.use(homeRouter);
     app.use('/patients', patientRouter);
-    app.use('/schedule', appointmentRouter);
+    // app.use('/schedule', appointmentRouter);
     app.use(errorHandler);
 }
