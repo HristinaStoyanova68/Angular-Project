a8af8919b110e32b0346e309974bc5a90ad91f3c
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-22 03:26:02 +0200

    add main-routing.module.ts

--- client/src/app/main/main.module.ts
+++ client/src/app/main/main.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './main/main.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    MainComponent,
   ],
   imports: [
-    CommonModule,
+    CommonModule, MainRoutingModule,
   ],
   exports: [
     HomeComponent,
+    MainComponent,
   ]
 })
 export class MainModule { }
