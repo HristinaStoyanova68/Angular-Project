8aafdade4512be162ae39a6fbcc4ca3c7b0085c4
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-22 03:27:58 +0200

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ErrorComponent } from './error/error.component';
+import { MainModule } from './main/main.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CoreModule,
     SharedModule,
+    MainModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
