1b01c17fcee647fc87e7ac0b8579ea907b30000c
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-22 03:28:36 +0200

    Update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './main/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '', component: HomeComponent },
+  {path: '**', redirectTo: '/404'}, 
+  {path: '404', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
