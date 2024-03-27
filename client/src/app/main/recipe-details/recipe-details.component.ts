a6d59e59e2dd738e63f2d96f948f12263a9000e7
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-28 00:02:52 +0200

    add recipe-details logic

--- client/src/app/main/recipe-details/recipe-details.component.ts
+++ client/src/app/main/recipe-details/recipe-details.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { Recipe } from 'src/app/types/recipe';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
   styleUrls: ['./recipe-details.component.css']
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit{
+  recipe = {} as Recipe;
+  isOwner: boolean = false;
 
+  constructor(private apiService: ApiService,
+    private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((data) => {
+      const recipeId = data['recipeId'];
+
+      this.apiService.getRecipeById(recipeId).subscribe((currentRecipe) => {
+        console.log({currentRecipe});
+        
+        this.recipe = currentRecipe;
+      })
+
+    })
+  }
 }
