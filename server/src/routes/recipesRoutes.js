70283b72ecfd23e92f276f53382eb85c0bb39be8
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-27 15:07:40 +0200

    Update recipesRoutes.js

--- server/src/routes/recipesRoutes.js
+++ server/src/routes/recipesRoutes.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 
 const recipesController = require('../controllers/recipesController');
 
+router.get('/:collectionName', recipesController.getAllRecipesForCollection);
+router.get('/:recipeId', recipesController.getRecipeById);
+router.get('/my-recipes', recipesController.getMyRecipes);
 router.post('/add-recipe', recipesController.addRecipe);
 
 module.exports = router;
