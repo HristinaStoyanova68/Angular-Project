eb5b8626963153ec65222f49274e4409a88bb828
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-28 00:01:52 +0200

    Update add-recipe.component.ts

--- client/src/app/main/add-recipe/add-recipe.component.ts
+++ client/src/app/main/add-recipe/add-recipe.component.ts
@@ -7,6 +7,8 @@ import { ApiService } from 'src/app/api.service';
   styleUrls: ['./add-recipe.component.css']
 })
 export class AddRecipeComponent {
+  allIngredients = [] as String[];
+
     constructor(private apiService: ApiService) {}
 
     addRecipe(ev: Event, recipeName: string) {
@@ -20,3 +22,42 @@ export class AddRecipeComponent {
     addInstruction() {}
 
 }
+
+
+// <div class="form-group ingredients">
+// <label for="ingredientQty">Ingredients:</label>
+// <div id="ingredients-container">
+//     <input type="number" min="0" name="ingredientQty" id="ingredientQty" [(ngModel)]="ingredientQty">
+//     <input type="text" name="ingredientName" id="ingredientName" [(ngModel)]="ingredientName">
+//     <button type="button" (click)="addIngredient()">Add Ingredient</button>
+// </div>
+// </div>
+// <div *ngFor="let ingredient of ingredients">
+// {{ ingredient.qty }} - {{ ingredient.name }}
+// </div>
+
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-your-component',
+//   templateUrl: './your-component.component.html',
+//   styleUrls: ['./your-component.component.css']
+// })
+// export class YourComponent {
+//   ingredientQty: number;
+//   ingredientName: string;
+//   ingredients: { qty: number, name: string }[] = [];
+
+//   addIngredient() {
+//     if (this.ingredientQty && this.ingredientName) {
+//       this.ingredients.push({ qty: this.ingredientQty, name: this.ingredientName });
+//       // Изчистване на стойностите на инпут полетата
+//       this.ingredientQty = null;
+//       this.ingredientName = '';
+//     }
+//   }
+// }
+
