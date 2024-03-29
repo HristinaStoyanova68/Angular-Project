73cc9749e9828ee3f327801e2064506abafcac44
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-29 16:46:21 +0200

    Update userRoutes.js

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,16 +1,13 @@
 const router = require('express').Router();
 
 const userController = require('../controllers/userController');
-// const { loginValidation, registerValidation } = require('../middlewares/userValidator');
-// const { registerValidation } = require('../middlewares/userValidator');
-// const loginLimiter = require('../middlewares/loginLimiter');
+const { loginValidation, registerValidation } = require('../middlewares/userValidator');
+const loginLimiter = require('../middlewares/loginLimiter');
 const verifyJWT = require('../middlewares/verifyJWT');
 
 
-// router.post('/login', loginValidation, loginLimiter, userController.login);
-router.post('/login', userController.login);
-// router.post('/register', registerValidation, userController.register);
-router.post('/register', userController.register);
-router.post('/logout', userController.logout);
+router.post('/login', loginValidation, loginLimiter, userController.login);
+router.post('/register', registerValidation, userController.register);
+router.post('/logout', verifyJWT, userController.logout);
 
 module.exports = router;
