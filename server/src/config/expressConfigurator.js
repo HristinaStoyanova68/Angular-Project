be03aeff21c40b3ec7ab0a906da6ce045415feeb
HRISTINA STOYANOVA <viara68@abv.bg>
2024-03-29 16:44:02 +0200

    update express and routes configurators

--- server/src/config/expressConfigurator.js
+++ server/src/config/expressConfigurator.js
@@ -5,13 +5,20 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const corsOptions = require('./corsOptions');
-const { logger } = require('../middlewares/logger');
+const logger = require('../middlewares/logger');
 
 function expressConfigurator(app) {
     console.log(`Node.js execution mode: ${process.env.NODE_ENV}`);
 
     app.use(logger);
     app.use(cors(corsOptions));
+    app.use(express.static('public', {
+        setHeaders: (res, path, stat) => {
+            if (path.endsWith('.js')) {
+                res.set('Content-Type', 'application/javascript')
+            }
+        }
+    }));
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
     app.use(cookieParser());
--- server/src/config/routesConfigurator.js
+++ server/src/config/routesConfigurator.js
@@ -1,10 +1,8 @@
-const homeRouter = require('../routes/homeRoutes');
 const userRouter = require('../routes/userRoutes');
 const recipesRouter = require('../routes/recipesRoutes');
 const errorHandler = require('../middlewares/errorHandler');
 
 module.exports = (app) => {
-    app.use(homeRouter);
     app.use('/users', userRouter);
     app.use('/recipes', recipesRouter);
     app.use(errorHandler);
