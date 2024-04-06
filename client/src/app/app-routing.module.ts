98964dc31221502b289a47a8447711584cbc590f
HRISTINA STOYANOVA <viara68@abv.bg>
2024-04-07 02:24:12 +0300

    add lazy loading
    user.module
    main.module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { HomeComponent } from './main/home/home.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
+  {path: 'auth', loadChildren: () => import('./user/user.module').then((m) => m.UserModule)},
+  {path: 'site', loadChildren: () => import('./main/main.module').then((m) => m.MainModule)},
   {path: '**', redirectTo: '/404'}, 
   {path: '404', component: ErrorComponent}
 ];
