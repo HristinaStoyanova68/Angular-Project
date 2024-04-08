f54131417184e4d2af8010c7baa6f1514862de0b
HRISTINA STOYANOVA <viara68@abv.bg>
2024-04-08 15:16:22 +0300

    Update verifyJWT.js

--- server/src/middlewares/verifyJWT.js
+++ server/src/middlewares/verifyJWT.js
@@ -1,43 +1,43 @@
-const jwt = require('../lib/jwt');
+const jwt = require("../lib/jwt");
 
 const verifyJWT = async (req, res, next) => {
-    
-    const token = req.headers.cookie.split('=')[1] || '';
 
-    if (!token) {
+  const token = req.cookies[process.env.AUTH_COOKIE_NAME] || "";
 
-        delete req.headers['Authorization'] || delete req.headers['authorization'];
-        res.clearCookie(process.env.AUTH_COOKIE_NAME);
-    
-       console.log('Token is missing!');
-    
-        return res.status(401).json({ message: 'Token is missing!' });
+  if (!token) {
+    delete req.headers["Authorization"] || delete req.headers["authorization"];
+    res.clearCookie(process.env.AUTH_COOKIE_NAME);
+
+    console.log("Token is missing!");
+
+    return res.status(401).json({ message: "Token is missing!" });
+  }
+
+  try {
+    const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    req.user = decoded.UserInfo; // req.user = {_id, username, email}
+    req.user.accessToken = token; //Now req.user = {_id, username, email, accessToken}
+
+    next();
+  } catch (error) {
+    delete req.headers["Authorization"] || delete req.headers["authorization"];
+
+    if (error.name === "TokenExpiredError") {
+      res.clearCookie(process.env.AUTH_COOKIE_NAME);
+
+      return res.status(401).json({ message: "Token has expired!" });
+    } else {
+      console.log(error.name);
+      console.log("Problem with token verification!");
+
+      res.clearCookie(process.env.AUTH_COOKIE_NAME);
+
+      return res
+        .status(403)
+        .json({ message: "Problem with token verification!" });
     }
-
-    try {
-        const decoded = await jwt.verify(token, process.env.Access_Token_Secret);
-
-        req.user = decoded.UserInfo; // req.user = {_id, username, email}
-        req.user.accessToken = token; //Now req.user = {_id, username, email, accessToken}
-
-        next();
-        
-    } catch (error) {
-        delete req.headers['Authorization'] || delete req.headers['authorization'];
-        
-        if (error.name === 'TokenExpiredError') {
-            res.clearCookie(process.env.AUTH_COOKIE_NAME);
-
-            return res.status(401).json({message: 'Token has expired!'});
-        } else {
-            console.log(error.name);
-            console.log('Problem with token verification!');
-            
-            res.clearCookie(process.env.AUTH_COOKIE_NAME);
-
-            return res.status(403).json({message: 'Problem with token verification!'});
-        }
-    }
+  }
 };
 
 module.exports = verifyJWT;
