a3ae0c7b9e85fb4b617bc26005c1d9928151884d
HRISTINA STOYANOVA <viara68@abv.bg>
2024-04-12 16:48:24 +0300

    Update cacheControl.js

--- server/src/middlewares/cacheControl.js
+++ server/src/middlewares/cacheControl.js
@@ -1,6 +1,10 @@
 const cacheControl = (req, res, next) => {
+    console.log('here1');
   if (req.url === "/login" || req.url === "/logout") {
+    console.log('here2');
+
     res.setHeader("Cache-Control", "no-cache");
+
   }
   next();
 };
